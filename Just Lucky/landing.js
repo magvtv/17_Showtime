8b4466115fafd2b455292629854ebecf2945889d
magvtv <magutujr@gmail.com>
2023-06-07 20:01:35 +0300

    added js file, made the intro text better on loading

---
+++ Just Lucky/landing.js
@@ -0,0 +1,21 @@
+
+const introPhrase = document.getElementById('intro');
+
+const loadingPhrases = [
+    "Loading the thrills and spills from big screens...",
+    "Grabbing, from Hollywood, the finest gems for you...",
+    "Fetching your blockbuster entertainment...",
+    "Hang tight. We're about to bless your sight...",
+    "Hoisting up your curtains. Stay tuned...",
+    "Unlocking your cinema's best experience..."
+];
+
+
+const getRandomIntro = () => {
+    const phrases = loadingPhrases.length
+    let randomIndex = Math.floor(Math.random() * phrases);
+    return loadingPhrases[randomIndex];
+}
+
+const anyPhrase = getRandomIntro()
+introPhrase.innerText = `${anyPhrase}`
