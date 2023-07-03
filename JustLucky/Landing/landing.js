f352d90e031a1f87726d8cd8cb92e5aa8be1e6b9
magvtv <magutujr@gmail.com>
2023-07-03 20:38:45 +0300

    not sure about landing.js

--- JustLucky/Landing/landing.js
+++ JustLucky/Landing/landing.js
@@ -1,55 +1,78 @@
-const page = document.body;
-const content = document.getElementById('preloader');
+// const page = document.body
+// const content = page.getElementById('preloader');
 
-const slider = document.querySelectorAll('.banner__section-slider')
-const slideNumbers = document.querySelector('.slider__numbers')
+// Read the contents of the config.json file
+const fs = require('fs');
+const configData = fs.readFileSync('config.json', 'UTF-8');
+// Parse the JSON data
+const config = JSON.parse(configData);
+// Access the API key
+const apiKey = config.apiKey;
+const apiUrl = 'https://www.themoviedb.org/movie/now-playing'
+const urlWithApiKey = `${apiUrl}?api_key=${apiKey}`
+
+// const slider = document.querySelectorAll('.banner__section-slider')
+// const slideNumbers = document.querySelector('.slider__numbers')
+
+// /* 
+//    preloader functions
+//*/
+// function preloader(page) {
+//     // Create a curtain element.
+//     // let curtain = document.createElement("div");
+//     content.style.width = "100%";
+//     content.style.height = "100%";
+//     content.style.background = "black";
+//     content.style.opacity = 0.5;
+
+//     // Add the curtain to the document.
+//     document.body.appendChild(content);
+
+//     // Start a timer to unveil the page.
+//     var timer = setInterval(function () {
+//         content.style.opacity -= 0.01;
+//         if (content.style.opacity <= 0) {
+//             document.body.removeChild(content);
+//             document.body.appendChild(page);
+//             clearInterval(timer);
+//         }
+//     }, 100);
+// }
 
 
-function preloader(page) {
-    // Create a curtain element.
-    // let curtain = document.createElement("div");
-    content.style.width = "100%";
-    content.style.height = "100%";
-    content.style.background = "black";
-    content.style.opacity = 0.5;
+/*
+    tried making the slider arrows work
+*/
 
-    // Add the curtain to the document.
-    document.body.appendChild(content);
+// slider.forEach((slide, index) => {
+//     const slideNumber = document.createElement('span');
+//     slideNumber.textContent = index + 1
+//     slideNumber.classList.add('slide-number');
+//     if (index === 0) {
+//         slideNumber.classList.add('current-slide')
+//     }
+//     slideNumbers.appendChild(slideNumber)
+// })
 
-    // Start a timer to unveil the page.
-    var timer = setInterval(function () {
-        content.style.opacity -= 0.01;
-        if (content.style.opacity <= 0) {
-            document.body.removeChild(content);
-            document.body.appendChild(page);
-            clearInterval(timer);
-        }
-    }, 100);
-}
+// const prevSliderButton = document.getElementById('prev'), nextSliderButton = document.getElementById('next')
+// let currentSlide = document.getElementsByClassName('current-slide')
 
+// function nextSlide() {
+//     currentSlide++
+//     if (currentSlide >= 10) {
+//         currentSlide = 0
+//     }
+// }
 
-slider.forEach((slide, index) => {
-    const slideNumber = document.createElement('span');
-    slideNumber.textContent = index + 1
-    slideNumber.classList.add('slide-number');
-    if (index === 0) {
-        slideNumber.classList.add('current-slide')
-    }
-    slideNumbers.appendChild(slideNumber)
-})
+/*
+    adding tmdb integration to the website
+*/
+fetch(urlWithApiKey)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => {
+        console.error("Something is wrong\n", error)
 
-const prevSliderButton = document.getElementById('prev'), nextSliderButton = document.getElementById('next')
-let currentSlide = document.getElementsByClassName('current-slide')
-
-function nextSlide() {
-    currentSlide++
-    if (currentSlide >= 10) {
-        currentSlide = 0
-    }
-}
-
-nextSliderButton.addEventListener('click', nextSlide)
-
-function showSlide(slideNumber) {
-
-}
+    })
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 17_Showtime: Cinema Landing Page
 
-- The codename is how I serially categorize my project on my system. Anyway, this is a cinema landing page project built using React.js. It provides an interactive and user-friendly interface for movie enthusiasts to explore movies, view showtimes, and book tickets. 
+- The codename is how I serially categorize my project on my system. Anyway, this is a cinema landing page project built.  It provides an interactive and user-friendly interface for movie enthusiasts to explore movies, view showtimes, and book tickets. 
 - Because Century Cinemax sounds a bit long (i feel it will pick up though), lately i have been thinking of CineMagic and MovieMania 
 
 ## Features
@@ -10,41 +10,17 @@
 - **Theater Information**: View information about the theaters, including their names, locations, and seating capacity.
 - **Booking**: Book tickets for preferred showtimes by selecting the desired movie, theater, and available seats.
 
-## Installation
-
-1. Clone the repository: `git clone https://github.com/your-username/cinema-landing-page.git`
-2. Navigate to the project directory: `cd cinema-landing-page`
-3. Install dependencies: `npm install`
-
-## Usage
-
-1. Start the development server: `npm start` (of course you know)
-2. Open your browser and visit `http://localhost:3000` to access the cinema landing page.
 
 ## Technologies Used
-
-- React.js: JavaScript library for building user interfaces
 - HTML5 and CSS3: Markup and styling of the application
 - Bootstrap: Front-end framework for responsive and mobile-first designs
 
-## Contributing
-
-Contributions are welcome! If you would like to contribute to this project, please follow these steps:
-
-1. Fork the repository.
-2. Create a new branch: `git checkout -b feature/your-feature`
-3. Make your changes and commit them: `git commit -m 'Add some feature'`
-4. Push the changes to your forked repository: `git push origin feature/your-feature`
-5. Open a pull request on the main repository.
 
 ## License
 
 This project is licensed under the [Creative Commons License](LICENSE).
 >I am sure to change the license
 
-## Acknowledgments
 
-Thanks to the creators of React.js and other open-source libraries used in this project. Special thanks to the contributors and supporters.
-Feel free to explore the cinema landing page and provide feedback. Enjoy your in-website experience!
-
+## Screenshot Samples
 ![Screenshot from 2023-06-07 20-04-31](https://github.com/magvtv/17_Showtime/assets/62017731/ccaada38-cec5-4b99-b57d-16e14f62e0f4)
